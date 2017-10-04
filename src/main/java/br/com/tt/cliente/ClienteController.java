91bd24d0dad61944c82d0cd97684f86d08770d33
Paulo Heck <pauloheck@gmail.com>
2017-10-03 21:09:07 -0300

    ajuste no cliente

--- src/main/java/br/com/tt/cliente/ClienteController.java
+++ src/main/java/br/com/tt/cliente/ClienteController.java
@@ -10,10 +10,10 @@ public class ClienteController {
 
 	@GetMapping
 	String cliente() {
-		return "cliente/index";
+		return "/cliente/index";
 	}
 	@GetMapping("/cadastro")
 	String cadastro() {
-		return "cliente/cadastro";
+		return "/cliente/cadastro";
 	}
 }
